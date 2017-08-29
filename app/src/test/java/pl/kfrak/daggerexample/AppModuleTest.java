bb8f4866acf393d744254e1108497374d9f2903e
rhquee <kfrak@proarray.com>
2017-08-29 20:42:53 +0200

    test of tests

---
+++ app/src/test/java/pl/kfrak/daggerexample/AppModuleTest.java
@@ -0,0 +1,28 @@
+package pl.kfrak.daggerexample;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by RENT on 2017-08-29.
+ */
+public class AppModuleTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void provideApplication() throws Exception {
+
+    }
+
+}
